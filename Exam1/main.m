06b7774290ccfcaa26686f4512c722b560b0e703
Carlos <carlos.V@carloss-macbook-pro.local>
2016-08-31 13:23:16 -0500

    Initial Commit

---
+++ Exam1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exam1
+//
+//  Created by Carlos E. Villalpando on 8/31/16.
+//  Copyright © 2016 Organo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
